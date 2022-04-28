823cf7f451e7e02127fda2c2175f70567d1331d1
Sam Maya <samuelmaya112@gmail.com>
2022-04-28 13:43:20 -0400

    new solutions

--- solutions.js
+++ solutions.js
@@ -1,1 +1,48 @@
-//test
+// In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+// For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+// If you can, try writing it in only one line of code.
+
+function findMultiples(integer, limit) {
+  //your code here
+    let answer = []
+    let i = 1
+    while (i * integer <= limit){
+        answer.push(i * integer)
+        i ++
+    }
+    return answer
+}
+
+
+
+
+// If you've completed this kata already and want a bigger challenge, here's the 3D version
+
+// Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+
+// There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+
+// +---+                                       +---+
+// |   |                                       |   |
+// +---+                                       +---+
+// +---++---+     +---+              +---++---++---+
+// |   ||   |     |   |   -->        |   ||   ||   |
+// +---++---+     +---+              +---++---++---+
+// +---++---++---++---+         +---++---++---++---+
+// |   ||   ||   ||   |         |   ||   ||   ||   |
+// +---++---++---++---+         +---++---++---++---+
+// Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+
+// Examples (input -> output:
+// * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+// * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+
+const flip=(d, c)=>{
+  if (d.toLowerCase() === 'r'){
+    c = c.sort(function (a, b) { return a - b; })
+  }else if (d.toLowerCase() === 'l'){
+    c = c.sort(function (a, b) {  return b - a; })
+  } return c
+}
