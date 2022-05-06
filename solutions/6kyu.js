ef6b05e6b36019ba4d6da9707d5b7cf0ac610dd5
Sam Maya <samuelmaya112@gmail.com>
2022-05-06 00:14:08 -0400

    new solutions

---
+++ solutions/6kyu.js
@@ -0,0 +1,27 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+    let num = String(n)
+    while (num.length > 1){
+      num = String(num.split('').map( x => Number(x)).reduce((acc,c) => acc + c ))
+    }
+    return Number(num)
+}
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string){
+    let strArr = string.split(' ')
+    let ans = strArr.map( x => (x.length >= 5 ? x.split('').reverse().join('') : x ))
+    return ans.join(' ')
+}
