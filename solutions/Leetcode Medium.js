85ce421044acb90c4f2ce75db71b807ddfcfac5f
Sam Maya <samuelmaya112@gmail.com>
2022-05-15 00:16:56 -0400

    New medium leetcode solution

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -30,3 +30,26 @@ var lengthOfLongestSubstring = function(s) {
   }
   return max
 };
+
+
+// 49. Group Anagrams
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+// Example 1:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+var groupAnagrams = function(strs) {
+  let map = {}
+  let sorted = strs.map( str => str.split('').sort().join(''))
+  for (let i = 0; i < strs.length; i++){
+      map[sorted[i]] === undefined ? map[sorted[i]] = [strs[i]] : map[sorted[i]].push(strs[i]);
+  }
+  return Object.values(map)
+};
