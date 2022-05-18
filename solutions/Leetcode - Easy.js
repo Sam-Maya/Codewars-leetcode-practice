38b60d514fe8accb57f46741ba739d0ffffc849f
Sam Maya <samuelmaya112@gmail.com>
2022-05-17 22:11:41 -0400

    new leetcode Medium

--- solutions/Leetcode - Easy.js
+++ solutions/Leetcode - Easy.js
@@ -138,3 +138,19 @@ var containsDuplicate = function(nums) {
         }
     return ans
 };
+
+// Valid Palindrome
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+//solution with no regex
+
+var isPalindrome = function(s) {
+    let letters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let noSpace = s.toLowerCase().split('').filter(x => letters.split('').includes(x))
+    return noSpace.join('') === noSpace.reverse().join('') ? true : false
+};
