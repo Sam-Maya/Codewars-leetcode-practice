713c6a3dda09eac8c1415232c4d3de0ff5687ea6
Sam Maya <samuelmaya112@gmail.com>
2022-05-19 18:28:22 -0400

    new medium leetcode

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -121,3 +121,32 @@ var productExceptSelf = function(nums) {
   }
   return ans
 };
+
+
+// Longest Consecutive Sequence
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+// Example 1:
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+var longestConsecutive = function(nums) {
+  if(nums === null || nums.length === 0){
+      return 0
+  }
+  let max = 1;
+  let numsSet = new Set(nums);
+  for (let num of numsSet){
+      if (numsSet.has(num - 1)) continue;
+      
+      let currentNum = num
+      let currentMax = 1
+      while (numsSet.has(currentNum + 1)){
+          currentMax++;
+          currentNum++;
+          max = Math.max(max, currentMax)
+      }
+  }
+  return max
+};
