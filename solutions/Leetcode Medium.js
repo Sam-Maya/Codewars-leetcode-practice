7f129daed9ea7bf15aa7e8096b270694264c700a
Sam Maya <samuelmaya112@gmail.com>
2022-05-20 18:36:23 -0400

    new mediums solution

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -150,3 +150,39 @@ var longestConsecutive = function(nums) {
   }
   return max
 };
+
+
+// 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+// Example 1:
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+
+//optimal solution
+function threeSum(nums){
+  nums.sort((a,b) => a-b)
+  let ans = []
+  for(let i = 0; i < nums.length; i++){
+      if(i > 0 && nums[i] === nums[i - 1]) continue;
+      let target = 0 - nums[i];
+      let left = i + 1;
+      let right = nums.length - 1;
+      while(left < right){
+          let sum = nums[left] + nums[right];
+          if(sum > target){
+              right--;
+          }else if(sum < target){
+              left++;
+          }else{
+              ans.push([nums[left], nums[right], nums[i]])
+              while(nums[left] === nums[left + 1]) left++;
+              while(nums[right] === nums[right - 1]) right--;
+              left++;
+              right--;
+              
+          }
+      }
+  }
+  return ans
+};
