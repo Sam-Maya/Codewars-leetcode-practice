9e8847e222c193692d45ced214f777f98974b4ed
Sam Maya <samuelmaya112@gmail.com>
2022-05-22 00:58:16 -0400

    new leetcode mediium solution

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -219,3 +219,18 @@ var maxArea = function(height) {
   }
   return max
 };
+
+//more optimal O(n) solution 
+var maxArea = function(height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right){
+      let current = Math.min(height[left], height[right]) * (right - left);
+      max = Math.max(max, current);
+      if (height[left] < height[right]){
+          left++;
+      }else right--;
+  }
+  return max
+};
