54925b3a8af39dbfa7a87828a0808cb410f31d88
Sam Maya <samuelmaya112@gmail.com>
2022-05-24 20:27:32 -0400

    add

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -198,7 +198,8 @@ function threeSum(nums){
 // Output: 49
 // Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-// Brute force solution, exceeds leetcode time limit but...it does work
+
+// Brute force solution, exceeds leetcode time limit but... it does work
 var maxArea = function(height) {
   if(height.length === 2){
       return Math.min(...height) * 1
