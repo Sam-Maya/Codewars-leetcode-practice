7903f7c1bd584e9874ff8fa8ed70a4a323f24ef8
Sam Maya <samuelmaya112@gmail.com>
2022-06-04 01:21:53 -0400

    new leetcode easy

--- solutions/Leetcode - Easy.js
+++ solutions/Leetcode - Easy.js
@@ -178,3 +178,38 @@ var searchInsert = function(nums, target) {
         }
     }
 };
+
+
+// Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Example 1:
+// Input: s = "()"
+// Output: true
+// Example 2:
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+// Input: s = "(]"
+// Output: false
+
+//O(n) time and space
+var isValid = function(s) {
+    let map = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
+    }
+    let stack = [];
+
+    for (char of s){
+        if (map[char] !== undefined){
+            stack.push(map[char]);
+        }else if(stack.length !== 0 && stack[stack.length-1] === char){
+            stack.pop();
+        }else return false
+    }
+    return stack.length === 0
+}
