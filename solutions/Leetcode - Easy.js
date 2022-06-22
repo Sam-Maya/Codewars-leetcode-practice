f41b4aa3f44e877b2bf6453de5902fd708146d7e
Sam Maya <samuelmaya112@gmail.com>
2022-06-22 13:09:18 -0400

    new leetcode easy - linkedlists

--- solutions/Leetcode - Easy.js
+++ solutions/Leetcode - Easy.js
@@ -240,3 +240,20 @@ var search = function(nums, target) {
     }
     return -1
 };
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+var reverseList = function(head) {
+    let previous = null;
+    let current = head;
+    while(current !== null){
+        let next = current.next
+        current.next = previous
+        previous = current
+        current = next
+    }
+    return previous
+}
