d6a62804315feb32ce8ec354cd76b8137c25c6aa
Sam Maya <samuelmaya112@gmail.com>
2022-07-07 11:16:47 -0400

    New leetcode medium

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -335,3 +335,50 @@ var findMin = function(nums) {
         }
     }
 };
+
+
+// You are given a 0-indexed binary string s which represents the types of buildings along a street where:
+// s[i] = '0' denotes that the ith building is an office and
+// s[i] = '1' denotes that the ith building is a restaurant.
+// As a city official, you would like to select 3 buildings for random inspection. However, to ensure variety, no two consecutive buildings out of the selected buildings can be of the same type.
+// For example, given s = "001101", we cannot select the 1st, 3rd, and 5th buildings as that would form "011" which is not allowed due to having two consecutive buildings of the same type.
+// Return the number of valid ways to select 3 buildings.
+// Example 1:
+// Input: s = "001101"
+// Output: 6
+// Explanation: 
+// The following sets of indices selected are valid:
+// - [0,2,4] from "001101" forms "010"
+// - [0,3,4] from "001101" forms "010"
+// - [1,2,4] from "001101" forms "010"
+// - [1,3,4] from "001101" forms "010"
+// - [2,4,5] from "001101" forms "101"
+// - [3,4,5] from "001101" forms "101"
+// No other selection is valid. Thus, there are 6 total ways.
+
+var numberOfWays = function(s) {
+    let validSets = 0
+    
+    let afterZeros = 0
+    let afterOnes = 0
+    
+    let beforeZeros = 0
+    let beforeOnes = 0
+    
+    for (let num of s){
+        num == 0 ? afterZeros++ : afterOnes++
+    }
+    
+    for (let num of s){
+        if (num == 0){
+            afterZeros--
+            beforeZeros++
+            validSets += (afterOnes * beforeOnes)
+        } else{
+            afterOnes--
+            beforeOnes++
+            validSets += (afterZeros * beforeZeros)
+        }
+    }
+    return validSets
+};
