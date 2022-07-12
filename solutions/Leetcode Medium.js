4866150184a8a931498b95c852472b29a49cc728
Sam Maya <samuelmaya112@gmail.com>
2022-07-12 16:40:16 -0400

    New leetcode Medium

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -382,3 +382,35 @@ var numberOfWays = function(s) {
     }
     return validSets
 };
+
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Example 1:
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+var addTwoNumbers = function(l1, l2) {
+    let prevNode = new ListNode()
+    let head = prevNode
+    let carry = 0
+    while (l1 || l2 || carry !== 0){
+        let val1 = 0
+        let val2 = 0
+        if (l1){
+            val1 = l1.val
+            l1 = l1.next
+        }
+        if (l2){
+            val2 = l2.val
+            l2 = l2.next
+        }
+        let sum = (val1 + val2 + carry) % 10
+        carry = Math.floor((val1 + val2 + carry) / 10)
+        let res = new ListNode(sum)
+        prevNode.next = res
+        prevNode = res
+    }
+    return head.next
+};
