939ccbd119164c324340d8b240cd3af770206e99
Sam Maya <samuelmaya112@gmail.com>
2022-07-18 14:16:47 -0400

    new leetcode medium

--- solutions/Leetcode Medium.js
+++ solutions/Leetcode Medium.js
@@ -414,3 +414,42 @@ var addTwoNumbers = function(l1, l2) {
     }
     return head.next
 };
+
+
+// You are given the head of a singly linked-list. The list can be represented as:
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+// Example 1:
+// Input: head = [1,2,3,4]
+// Output: [1,4,2,3]
+
+var reorderList = function(head) {
+    //finding the middle
+    let slow = head
+    let fast = head.next
+    while (fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    // reversing second half
+    let second = slow.next
+    slow.next = null
+    let prev = slow.next
+    while(second){
+        let temp = second.next
+        second.next = prev
+        prev = second
+        second = temp
+    }
+    // merging 2 lists
+    while (prev){
+        let temp1 = head.next
+        let temp2 = prev.next
+        head.next = prev
+        prev.next = temp1
+        head = temp1
+        prev = temp2
+    }
+};
